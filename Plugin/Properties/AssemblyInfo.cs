95c66bc66d605b46d1527736de733c91bd7b5b57
mik14a <mik14a@gmail.com>
2017-04-18 20:10:36 +0900

    Start CaperVisuaStudioExtension.

---
+++ Plugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CaperPlugin")]
+[assembly: AssemblyProduct("CaperPlugin")]
+[assembly: AssemblyCopyright("Copyright © 2017 mik14a <mik14a@gmail.com>")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,12 @@
+﻿Caper VisualStudio Extension
+============================
+
+Unofficial [Caper](https://github.com/jonigata/caper) VisualStudio Extension.
+
+## Licence
+
+[WTFPL](http://www.wtfpl.net/)
+
+## Author
+
+[mik14a](https://github.com/mik14a)
